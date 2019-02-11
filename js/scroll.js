eec5770a5419d767a4fbd021afd4b572e55429f4
Hypatech <unconfigured@null.spigotmc.org>
2019-02-11 20:28:48 +0000

    Added pulsing arrow-boye

--- js/scroll.js
+++ js/scroll.js
@@ -1,1 +1,10 @@
-//TODO - SNAP TO COLUMN
+$(document).ready(function(){
+    $(document).bind('wheel', function(){
+        var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
+        if(scrollBottom > 0){
+            $("#scrollarrow").stop().fadeIn();
+        } else{
+            $("#scrollarrow").stop().fadeOut();
+        }
+    });
+});
