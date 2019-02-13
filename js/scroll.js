c967e5669f7fb692e2e3c9649a40ed30320c66bb
Hypatech <silverma@hotmail.co.uk>
2019-02-13 09:42:20 +0000

    Changed arrow to scroll + 1 other
    Changed threshold for scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $(document).bind('wheel', function(){
-        var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
-        if(scrollBottom > 0){
+        if($(window).scrollTop() < 50){
             $("#scrollarrow").stop().fadeIn();
         } else{
             $("#scrollarrow").stop().fadeOut();
