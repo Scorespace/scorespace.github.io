257b7e928a1a989d00dcda92587238bf7c326ba2
Hypatech <unconfigured@null.spigotmc.org>
2019-08-16 22:51:41 +0100

    Updates

---
+++ precache-manifest.e5d07bee9d1b995e38449f97049e07e4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386827a09170f445c87d61440a78a43d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16387c3e968cbccb8a72",
+    "url": "/static/css/main.4a9fef60.chunk.css"
+  },
+  {
+    "revision": "6d95c28d99e647e2e847",
+    "url": "/static/js/2.d8c3d9e6.chunk.js"
+  },
+  {
+    "revision": "16387c3e968cbccb8a72",
+    "url": "/static/js/main.6b1a75d9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "418600b9d4a22de5c5b527c401b93b42",
+    "url": "/static/media/controller.418600b9.png"
+  },
+  {
+    "revision": "0df40b9e9442ddaaf98e4748dd2c9da0",
+    "url": "/static/media/envelope.0df40b9e.png"
+  },
+  {
+    "revision": "e70a71b9aef860802fa901304133f523",
+    "url": "/static/media/logo.e70a71b9.png"
+  },
+  {
+    "revision": "41d9b502ee354f08fd06cbb5c8eff9f5",
+    "url": "/static/media/mailbox.41d9b502.png"
+  },
+  {
+    "revision": "eaec57f4ae07369f043fb5ef848be351",
+    "url": "/static/media/pc.eaec57f4.png"
+  },
+  {
+    "revision": "9ffc34b116a2a9e3e6aca6a57237e797",
+    "url": "/static/media/scorespace_banner.9ffc34b1.png"
+  },
+  {
+    "revision": "25e637c2e6797fcafc84e1b512a6cb5d",
+    "url": "/static/media/space_background.25e637c2.png"
+  }
+]);
